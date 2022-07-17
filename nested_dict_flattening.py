c71dbb2d0c1d567adc3aef432e6fe0ef5428f9db
VasilijKolomiets <vasilij.kolomiets@gmail.com>
2022-07-17 15:46:23 +0300

    type conversion done

--- nested_dict_flattening.py
+++ nested_dict_flattening.py
@@ -7,6 +7,7 @@
     Development 5-th step. Composite output keys.
 
 """
+from datetime import datetime as dt
 
 
 def flatten_by_pattern(dict_in: dict, pattern_dict: dict, parent_key='') -> dict:
@@ -22,11 +23,14 @@ def flatten_by_pattern(dict_in: dict, pattern_dict: dict, parent_key='') -> dict
     out_dict = dict()
     for key, value in pattern_dict.items():
         if key in dict_in:
+            current_composite_key = parent_key + '.' + key
             print(F'{key=} {dict_in[key]=}')
             if value is True:                       # copying proper final value
-                out_dict.update({parent_key + '.' + key: dict_in[key]})
+                out_dict.update({current_composite_key: dict_in[key]})
+            elif callable(value):
+                out_dict.update({current_composite_key: value(dict_in[key])})
             elif isinstance(value, dict):
-                out_dict.update(flatten_by_pattern(dict_in[key], value, parent_key=parent_key + '.' + key))
+                out_dict.update(flatten_by_pattern(dict_in[key], value, current_composite_key))
             else:
                 assert False, F'What the "{value}"? Or "True" or dict !! '
 
@@ -38,14 +42,14 @@ if __name__ == '__main__':
         bdate='11.7.1993',
         last_name='Kovalenko',
         first_name='Shimon',
-        city=dict(id=3, title='Moscow'),
-        country=dict(id=77, title='Russia'),
-        schools=dict(id=378, year_from=2000, year_to=2012, name='School 33'),
-        last_seen=dict(time=True),
+        city=dict(id='3', title='Moscow'),
+        country=dict(id='77', title='Russia'),
+        schools=dict(id='378', year_from='2000', year_to='2012', name='School 33'),
+        last_seen=dict(time=1658061885.242944),
         universities=dict(
-            id=719,  name='MSU',
+            id='719',  name='MSU',
             faculty='Applied Mathematics',
-            graduation=dict(year_from=2013, year_to=2018),
+            graduation=dict(year_from='2013', year_to='2018'),
         )
     )
 
@@ -55,11 +59,11 @@ if __name__ == '__main__':
         first_name=True,
         city=dict(title=True),
         country=dict(title=True),
-        schools=dict(year_to=True),
-        last_seen=dict(time=True),
+        schools=dict(year_to=int),
+        last_seen=dict(time=dt.fromtimestamp),
         universities=dict(
             name=True,
-            graduation=dict(year_to=True),
+            graduation=dict(year_to=int),
         ),
     )
 
